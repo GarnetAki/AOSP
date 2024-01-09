2ba62b0a142418623e6b854fd7900b0bf7a85aa1
GarnetAki <RomeNightingale@gmail.com>
2024-01-09 14:09:22 +0300

    Del import

--- calculator-GarnetAki/app/src/main/java/ru/itmo/calculator/MainActivity.kt
+++ calculator-GarnetAki/app/src/main/java/ru/itmo/calculator/MainActivity.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
 import androidx.activity.ComponentActivity
-
 
 class MainActivity : ComponentActivity() {
 
